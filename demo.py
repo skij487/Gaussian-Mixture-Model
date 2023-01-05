56ba4a2b3ad2fdf7ed1de8fb1cb43840f158f565
shifu <skij99@gmail.com>
2023-01-05 09:46:26 -0500

    second

---
+++ demo.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import GMM2D
+
+
+POIs = np.loadtxt('POIs')
+mus, sigma, r, zs = GMM2D.GMM2D(POIs, 3, 'full', 25)
+color_r = [r[n]/np.sum(r[n]) for n in range(len(POIs))]
+for n in range(len(POIs)):
+    plt.plot(POIs[n][0], POIs[n][1], '.', color=(color_r[n][0],color_r[n][1],color_r[n][2]), markersize=4)
+plt.show()
